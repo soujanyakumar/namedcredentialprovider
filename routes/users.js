88b7ef67ade68b8113e3bd3fd675f2cda5276185
Kumar Kote <skote@salesforce.com>
2021-03-08 13:36:31 -0500

    first commit

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
 router.post('/', function (req, res, next) {
   console.log('headers : ' + JSON.stringify(req.headers));
   console.log('body : ' + JSON.stringify(req.body));
+  var userpass = new Buffer.from((req.headers.authorization || '').split(' ')[1] || '', 'base64').toString();
 
   res.send('respond with a resource');
 });
