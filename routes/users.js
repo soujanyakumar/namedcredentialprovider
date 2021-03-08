fedc9b44dea93ddbe135673dfc8f032a0fa18f88
Kumar Kote <skote@salesforce.com>
2021-03-08 13:21:15 -0500

    first commit

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/', function (req, res, next) {
+  console.log('headers : ' + JSON.stringify(req.headers));
+  console.log('body : ' + JSON.stringify(req.body));
+
   res.send('respond with a resource');
 });
 
